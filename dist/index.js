9332fb698b7d85e0be37623b2b44d96487c51549
Jonah Jackson <jonahjackson@uflip.com>
2023-09-18 11:38:45 -0500

    moved label issue to seperate js file

--- dist/index.js
+++ dist/index.js
@@ -9631,44 +9631,30 @@ function wrappy (fn, cb) {
 
 /***/ }),
 
-/***/ 1713:
+/***/ 3020:
 /***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
 
-// https://github.com/actions/javascript-action#update-the-action-code
+// https://github.com/actions/javascript-action
+// https://octokit.github.io/rest.js/v20#usage
 // https://github.com/actions/toolkit/blob/master/README.md
 
-const core = __nccwpck_require__(2186)
-const github = __nccwpck_require__(5438)
-
-const { wait } = __nccwpck_require__(1312)
+const core = __nccwpck_require__(2186);
+const github = __nccwpck_require__(5438);
 
 /**
  * The main function for the action.
  * @returns {Promise<void>} Resolves when the action is complete.
  */
-async function run() {
+async function labelIssue() {
   try {
-    const ms = core.getInput('milliseconds', { required: true })
-
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
-
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
-
     /**
      * We need to fetch all the inputs that were provided to our action
      * and store them in variables for us to use.
      **/
-    const owner = core.getInput('owner', { required: true })
-    const repo = core.getInput('repo', { required: true })
-    const issue_number = core.getInput('issue_number', { required: true })
-    const token = core.getInput('token', { required: true })
+    const owner = core.getInput('owner', { required: true });
+    const repo = core.getInput('repo', { required: true });
+    const issue_number = core.getInput('issue_number', { required: true });
+    const token = core.getInput('token', { required: true });
 
     /**
      * Now we need to create an instance of Octokit which will use to call
@@ -9678,23 +9664,68 @@ async function run() {
      * You can find all the information about how to use Octokit here:
      * https://octokit.github.io/rest.js/v18
      **/
-    const octokit = new github.getOctokit(token)
+    const octokit = new github.getOctokit(token);
 
     octokit.rest.issues.addLabels({
       owner,
       repo,
       issue_number,
       labels: ['test']
-    })
+    });
   } catch (error) {
     // Fail the workflow run if an error occurs
-    core.setFailed(error.message)
+    core.setFailed(error.message);
+  }
+}
+
+module.exports = { labelIssue };
+
+
+/***/ }),
+
+/***/ 1713:
+/***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
+
+// https://github.com/actions/javascript-action
+// https://octokit.github.io/rest.js/v20#usage
+// https://github.com/actions/toolkit/blob/master/README.md
+
+// packages
+const core = __nccwpck_require__(2186);
+
+// files
+const { labelIssue } = __nccwpck_require__(3020);
+const { wait } = __nccwpck_require__(1312);
+
+/**
+ * The main function for the action.
+ * @returns {Promise<void>} Resolves when the action is complete.
+ */
+async function run() {
+  try {
+    const ms = core.getInput('milliseconds', { required: true });
+
+    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
+    core.debug(`Waiting ${ms} milliseconds ...`);
+
+    // Log the current timestamp, wait, then log the new timestamp
+    core.debug(new Date().toTimeString());
+    await wait(parseInt(ms, 10));
+    core.debug(new Date().toTimeString());
+
+    // Set outputs for other workflow steps to use
+    core.setOutput('time', new Date().toTimeString());
+
+    labelIssue();
+  } catch (error) {
+    // Fail the workflow run if an error occurs
+    core.setFailed(error.message);
   }
 }
 
 module.exports = {
   run
-}
+};
 
 
 /***/ }),
@@ -9711,14 +9742,14 @@ module.exports = {
 async function wait(milliseconds) {
   return new Promise(resolve => {
     if (isNaN(milliseconds)) {
-      throw new Error('milliseconds not a number')
+      throw new Error('milliseconds not a number');
     }
 
-    setTimeout(() => resolve('done!'), milliseconds)
-  })
+    setTimeout(() => resolve('done!'), milliseconds);
+  });
 }
 
-module.exports = { wait }
+module.exports = { wait };
 
 
 /***/ }),
@@ -9903,9 +9934,9 @@ var __webpack_exports__ = {};
 /**
  * The entrypoint for the action.
  */
-const { run } = __nccwpck_require__(1713)
+const { run } = __nccwpck_require__(1713);
 
-run()
+run();
 
 })();
 
--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@
 // https://octokit.github.io/rest.js/v20#usage
 // https://github.com/actions/toolkit/blob/master/README.md
 
-const core = require('@actions/core')
-const github = require('@actions/github')
+// packages
+const core = require('@actions/core');
 
-const { wait } = require('./wait')
+// files
+const { labelIssue } = require('./label-issue');
+const { wait } = require('./wait');
 
 /**
  * The main function for the action.
@@ -13,50 +15,26 @@ const { wait } = require('./wait')
  */
 async function run() {
   try {
-    const ms = core.getInput('milliseconds', { required: true })
+    const ms = core.getInput('milliseconds', { required: true });
 
     // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    core.debug(`Waiting ${ms} milliseconds ...`);
 
     // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    core.debug(new Date().toTimeString());
+    await wait(parseInt(ms, 10));
+    core.debug(new Date().toTimeString());
 
     // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    core.setOutput('time', new Date().toTimeString());
 
-    /**
-     * We need to fetch all the inputs that were provided to our action
-     * and store them in variables for us to use.
-     **/
-    const owner = core.getInput('owner', { required: true })
-    const repo = core.getInput('repo', { required: true })
-    const issue_number = core.getInput('issue_number', { required: true })
-    const token = core.getInput('token', { required: true })
-
-    /**
-     * Now we need to create an instance of Octokit which will use to call
-     * GitHub's REST API endpoints.
-     * We will pass the token as an argument to the constructor. This token
-     * will be used to authenticate our requests.
-     * You can find all the information about how to use Octokit here:
-     * https://octokit.github.io/rest.js/v18
-     **/
-    const octokit = new github.getOctokit(token)
-
-    octokit.rest.issues.addLabels({
-      owner,
-      repo,
-      issue_number,
-      labels: ['test']
-    })
+    labelIssue();
   } catch (error) {
     // Fail the workflow run if an error occurs
-    core.setFailed(error.message)
+    core.setFailed(error.message);
   }
 }
 
 module.exports = {
   run
-}
+};
