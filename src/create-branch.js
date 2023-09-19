2219a9c333b59686d569f1bdda9d0a82488a0a1d
Jonah Jackson <jonahjackson@uflip.com>
2023-09-19 10:52:08 -0500

    test create branch #2

--- src/create-branch.js
+++ src/create-branch.js
@@ -29,11 +29,41 @@ async function createBranch() {
      **/
     const octokit = new github.getOctokit(token);
 
+    const ref = 'development';
+
+    /* 
+    {
+      "ref": "refs/heads/featureA",
+      "node_id": "MDM6UmVmcmVmcy9oZWFkcy9mZWF0dXJlQQ==",
+      "url": "https://api.github.com/repos/octocat/Hello-World/git/refs/heads/featureA",
+      "object": {
+        "type": "commit",
+        "sha": "aa218f56b14c9653891f9e74264a383fa43fefbd",
+        "url": "https://api.github.com/repos/octocat/Hello-World/git/commits/aa218f56b14c9653891f9e74264a383fa43fefbd"
+      }
+    }
+    
+    */
+
+    const devBranch = await octokit.request(
+      `GET /repos/${owner}/${repo}/git/ref/${ref}`,
+      {
+        owner,
+        repo,
+        ref,
+        headers: {
+          'X-GitHub-Api-Version': '2022-11-28'
+        }
+      }
+    );
+
+    const sha = await devBranch?.object?.sha;
+
     await octokit.request(`POST /repos/${owner}/${repo}/git/refs`, {
       owner,
       repo,
       ref: 'refs/heads/featureA',
-      sha: 'aa218f56b14c9653891f9e74264a383fa43fefbd',
+      sha,
       headers: {
         'X-GitHub-Api-Version': '2022-11-28'
       }
